d79fd38f06ed3210197536367dbc1b71a8132f33
Weym <weydsonmarinho@gmail.com>
2016-04-10 16:38:34 -0300

    Added files via upload

--- SistemaBiblioteca/src/repositorio/RepositorioLivro.java
+++ SistemaBiblioteca/src/repositorio/RepositorioLivro.java
@@ -22,35 +22,4 @@ public class RepositorioLivro {
 		return true;
 	}
 
-
-	public List<Livro> listarLivros() {
-		// TODO Auto-generated method stub
-		return this.listaLivros;
-	}
-	/*
-	public boolean deletarPessoaPorID(long id) {
-
-		for (Livro livro : listaLivros) {
-			if (livro.getId() == id) {
-				listaLivros.remove(livro);
-			}
-		}
-
-		return false;
-	}
-
-	public boolean alteraLivro(Livro livro) {
-
-		for (Livro livro1 : listaLivros) {
-			if (livro1.getId() == livro.getId()) {
-				
-				listaLivros.remove(livro1);
-				listaLivros.add(livro);
-				
-			}
-		}
-
-		return false;
-	}
-*/
 }
